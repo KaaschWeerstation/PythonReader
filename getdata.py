f813317d278c07744843874ed50192b00fcf896e
Ivo Gerner <i.gerner@outlook.com>
2020-02-11 15:40:18 +0100

    getdate.py now creates [stationID].csv file.

--- getdata.py
+++ getdata.py
@@ -19,14 +19,13 @@ def getData(stationID, filename):
             mDate = datetime(1970, 1, 1, 0, 0) + timedelta(bytesToInt(s[12:20]))
             index = 20
             stationData = {}
-            i = 1
 
             while index+23 <= s.size():
                 temperature = bytesToInt(s[index + 4:index + 6]) / 100
                 dewPoint = round((bytesToInt(s[index+4:index+6])/100 + bytesToInt(s[index+6:index+7])/10), 1)
                 fullDate = mDate + timedelta(seconds=(bytesToInt(s[index:index + 4])))
                 timeData = fullDate.strftime("%d-%m-%Y %H:%M:%S")
-                stationData[i] = {'time': timeData, 'temperature': temperature, 'humidity': humidty(temperature, dewPoint)}
+                # stationData[i] = {'time': timeData, 'temperature': temperature, 'humidity': humidty(temperature, dewPoint)}
                 writer.writerow([timeData, temperature, humidty(temperature, dewPoint)])
 
                 i += 1
@@ -36,4 +35,4 @@ def getData(stationID, filename):
     wd.close()
     return stationData
 
-print(getData(405750, 18298))
+getData(405750, 18298)
