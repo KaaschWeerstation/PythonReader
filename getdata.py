6531f9bee5561bda2ed308e2c2c6489adcc6c325
Ivo Gerner <i.gerner@outlook.com>
2020-02-11 16:44:01 +0100

    Created weatherdata.py, place in data folder of RaspberryProcessor, with stationid.csv (created by running stationcountry.py) and run to get a .csv file with all relevant data of relevant weatherstations.

--- getdata.py
+++ getdata.py
@@ -3,35 +3,38 @@ import math
 from mmap import *
 from datetime import *
 
+
 def bytesToInt(bytes):
-    return int.from_bytes(bytes, byteorder="big",signed=True)
+    return int.from_bytes(bytes, byteorder="big", signed=True)
+
 
 def humidty(temperature, dewPoint):
     actualVapourPressure = math.exp((17.625 * dewPoint) / (243.04 + dewPoint))
     standardVapourPressure = math.exp((17.625 * temperature) / (243.04 + temperature))
     return round(actualVapourPressure / standardVapourPressure * 100, 1)
 
+
 def getData(stationID, filename):
-    with open("venv/"+str(stationID)+".csv", 'w', newline='') as wd:
+    with open("venv/" + str(stationID) + ".csv", 'w', newline='') as wd:
         writer = csv.writer(wd)
         writer.writerow(["Time", "Temperature", "Humidity"])
-        with open("venv/"+str(stationID)+"/"+str(filename)+".wd", "rb", 0) as f, mmap(f.fileno(), 0, access=ACCESS_READ) as s:
+        with open("venv/" + str(stationID) + "/" + str(filename) + ".wd", "rb", 0) as f, mmap(f.fileno(), 0,
+                                                                                              access=ACCESS_READ) as s:
             mDate = datetime(1970, 1, 1, 0, 0) + timedelta(bytesToInt(s[12:20]))
             index = 20
-            stationData = {}
 
-            while index+23 <= s.size():
+            while index + 23 <= s.size():
                 temperature = bytesToInt(s[index + 4:index + 6]) / 100
-                dewPoint = round((bytesToInt(s[index+4:index+6])/100 + bytesToInt(s[index+6:index+7])/10), 1)
+                dewPoint = round((bytesToInt(s[index + 4:index + 6]) / 100 + bytesToInt(s[index + 6:index + 7]) / 10),
+                                 1)
                 fullDate = mDate + timedelta(seconds=(bytesToInt(s[index:index + 4])))
                 timeData = fullDate.strftime("%d-%m-%Y %H:%M:%S")
-                # stationData[i] = {'time': timeData, 'temperature': temperature, 'humidity': humidty(temperature, dewPoint)}
                 writer.writerow([timeData, temperature, humidty(temperature, dewPoint)])
 
                 index += 23
 
         f.close()
     wd.close()
-    return stationData
+
 
 getData(405750, 18298)
